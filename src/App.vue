619bf2a11749d963ae78a20c0bf37c92510b9a96
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-08-20 10:38:07 -0300

    Criando componente

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral @aoTemaAlterado="trocarTema"/>
+      <BarraLateral @aoTemaAlterado="trocarTema" />
     </div>
     <div class="column is-three-quarter conteudo">
+      <Notificacoes />
       <router-view></router-view>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import BarraLateral from './components/BarraLateral.vue';
-
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Notificacoes from "./components/Notificacoes.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     BarraLateral,
+    Notificacoes
   },
-  data () {
+  data() {
     return {
       modoEscuroAtivo: false
-    }
+    };
   },
   methods: {
-    trocarTema (modoEscuroAtivo: boolean) {
-      this.modoEscuroAtivo = modoEscuroAtivo
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
     }
   }
 });
---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="notificacoes">
+    <article class="message is-success">
+      <div class="message-header">Atenção!</div>
+      <div class="message-body">Notificação</div>
+    </article>
+    <article class="message is-warning">
+      <div class="message-header">Atenção!</div>
+      <div class="message-body">Notificação</div>
+    </article>
+    <article class="message is-danger">
+      <div class="message-header">Atenção!</div>
+      <div class="message-body">Notificação</div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NotificacoesAlura"
+});
+</script>
+
+<style scoped>
+.notificacoes {
+    position: absolute;
+    right: 0;
+    width: 300px;
+    padding: 8px;
+    z-index: 999;
+}
+</style>
