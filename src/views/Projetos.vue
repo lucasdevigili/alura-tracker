aa8ebef1b7f95f47dc12906c09c06256e1c78ff5
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-08-19 23:49:47 -0300

    setup da store

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -18,9 +18,9 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="projeto in projetos"  :key="projeto.id">
-            <td>{{ projeto.id }}</td>
-            <td>{{ projeto.nome }}</td>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
         </tr>
       </tbody>
     </table>
@@ -28,25 +28,27 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import IProjeto from "../interfaces/IProjeto";
+import { defineComponent, computed } from "vue";
+import { useStore } from "@/store";
 export default defineComponent({
   name: "ProjetosAlura",
   data() {
     return {
-      nomeDoProjeto: "",
-      projetos: [] as IProjeto[]
+      nomeDoProjeto: ""
     };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString()
-      };
-      this.projetos.push(projeto);
+      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
       this.nomeDoProjeto = "";
     }
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos)
+    };
   }
 });
 </script>
