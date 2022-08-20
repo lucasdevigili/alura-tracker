6163736883bbde80b0fce3027d5c39d889847be9
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-08-19 23:17:45 -0300

    Um estado para todos governar

---
+++ src/store/index.ts
@@ -0,0 +1,28 @@
+import IProjeto from "@/interfaces/IProjeto";
+import { createStore, Store } from "vuex";
+import { InjectionKey } from "vue";
+
+interface Estado {
+    projetos: IProjeto[]
+}
+
+export const key: InjectionKey<Store<Estado> = Symbol()
+
+export const store = createStore<Estado>({
+    state: {
+        projetos: [
+            {
+                id: new Date().toDateString(),
+                nome: 'TypeScript'
+            },
+            {
+                id: new Date().toDateString(),
+                nome: 'Vue'
+            },
+            {
+                id: new Date().toDateString(),
+                nome: 'Vuex'
+            }
+        ]
+    }
+})
