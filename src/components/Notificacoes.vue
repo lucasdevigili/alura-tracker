1b1b813b9511e40ed6691ceed233b2b1d952b959
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-08-20 11:14:22 -0300

    Notificação via store

--- src/components/Notificacoes.vue
+++ src/components/Notificacoes.vue
@@ -1,34 +1,45 @@
 <template>
   <div class="notificacoes">
-    <article class="message is-success">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Notificação</div>
-    </article>
-    <article class="message is-warning">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Notificação</div>
-    </article>
-    <article class="message is-danger">
-      <div class="message-header">Atenção!</div>
-      <div class="message-body">Notificação</div>
+    <article class="message"
+    :class="contexto[notificacao.tipo]"
+     v-for="notificacao in notificacoes" :key="notificacao.id">
+      <div class="message-header">{{ notificacao.titulo }}</div>
+      <div class="message-body">{{ notificacao.texto }}</div>
     </article>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+import { useStore } from "@/store";
+import { TipoNotificacao } from "../interfaces/INotificacao";
 
 export default defineComponent({
-  name: "NotificacoesAlura"
+  name: "NotificacoesAlura",
+  data() {
+    return {
+        contexto: {
+            [TipoNotificacao.SUCESSO]: 'is-success',
+            [TipoNotificacao.ATENCAO]: 'is-warning',
+            [TipoNotificacao.FALHA]: 'is-danger'
+        }
+    }
+  },
+  setup() {
+    const store = useStore();
+    return {
+      notificacoes: computed(() => store.state.notificacoes)
+    };
+  }
 });
 </script>
 
 <style scoped>
 .notificacoes {
-    position: absolute;
-    right: 0;
-    width: 300px;
-    padding: 8px;
-    z-index: 999;
+  position: absolute;
+  right: 0;
+  width: 300px;
+  padding: 8px;
+  z-index: 999;
 }
 </style>
